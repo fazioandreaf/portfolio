894b0bd6a824ce60cfbc70a5285b51e75eb401bd
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2023-11-29 13:54:30 +0100

    Fix hamburgher menu

--- src/components/HamburgerMenu/HamburgerMenu.tsx
+++ src/components/HamburgerMenu/HamburgerMenu.tsx
@@ -8,7 +8,7 @@ const HamburgerMenu = ({isOpen, onClick}: {isOpen: boolean; onClick: () => void}
 	const isMobile = useMediaQuery({maxWidth: 768});
 
 	return (
-		<div className={cn(styles['hamburgher-menu'], {[styles.open]: isOpen, 'd-none': !isMobile})} onClick={onClick}>
+		<div className={cn(styles['hamburgher-menu'], {[styles.open]: isOpen, 'd-none': isMobile})} onClick={onClick}>
 			<span className="line-1" />
 			<span className="line-2" />
 			<span className="line-3" />
